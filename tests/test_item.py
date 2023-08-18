74d1e9d4a7a722f08fc26fe3fdad00759f9be8dd
Aleksandr <ribakov.alex2013@yandex.ru>
2023-08-18 22:33:09 +0300

    add initialisation class Item, 2 funcs and test

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,14 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
+import pytest
+from src.item import *
+
+item1 = Item("phone", 1000, 5)
+
+
+def test_item():
+    # Testcase№1 test calculate
+    assert 1000 * 5 == item1.calculate_total_price()
+    # Testcase№2 test discount
+    Item.pay_rate = 0.8
+    item1.apply_discount()
+    assert item1.price == 800
