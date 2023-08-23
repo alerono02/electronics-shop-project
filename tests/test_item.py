98cfebf566aeeebc7ac3501d2be5e18dec6582de
Aleksandr <ribakov.alex2013@yandex.ru>
2023-08-23 15:32:24 +0300

    Add test for str and repr methods

--- tests/test_item.py
+++ tests/test_item.py
@@ -2,29 +2,38 @@
 import pytest
 from src.item import *
 
-item1 = Item("phone", 1000, 5)
 
-
+@pytest.fixture()
 def test_item():
+    item1 = Item("phone", 1000, 5)
+    return item1
+
+
+def test_first(test_item):
     # Testcase№1 test calculate
-    assert 1000 * 5 == item1.calculate_total_price()
+    assert 1000 * 5 == test_item.calculate_total_price()
     # Testcase№2 test discount
     Item.pay_rate = 0.8
-    item1.apply_discount()
-    assert item1.price == 800
+    test_item.apply_discount()
+    assert test_item.price == 800
 
 
-def test_second():
+def test_second(test_item):
     # Testcase№3 get name
-    assert item1.name == "phone"
+    assert test_item.name == "phone"
     # Testcase№4 set name
-    item1.name = "abcdefghijklmnop"
-    assert item1.name == 'abcdefghij'
+    test_item.name = "electronic_device"
+    assert test_item.name == 'electronic'
     # Testcase№5 instantiate from csv
-    Item.all=[]     # clean the list, bcs item1 instand in Item.all
+    Item.all = []  # clean the list, bcs item1 instand in Item.all
     Item.instantiate_from_csv()
     assert len(Item.all) == 5
     # Testcase№6 transform number
     assert Item.string_to_number('3') == 3
     assert Item.string_to_number('15.55') == 15
     assert Item.string_to_number('37.78456') == 37
+
+
+def test_third(test_item):
+    assert repr(test_item) == "Item('phone', 1000, 5)"
+    assert str(test_item) == 'phone'
